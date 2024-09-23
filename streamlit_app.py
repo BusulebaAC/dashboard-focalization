0b12d7ed75be9cd6ed1a62b7e41858031e3d0350
BusulebaAC <busulebaac@gmail.com>
2024-09-23 19:28:44 +0000

    cambio de focalizacion

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,20 +2,19 @@ import streamlit as st
 import pandas as pd
 import subprocess
 
+# Set the title and favicon that appear in the Browser's tab bar.
+st.set_page_config(page_title='Reporte del de focalización')  # Debe ser la primera línea de Streamlit
+
 # Ejecutar el script de limpieza
 try:
-    subprocess.run(['python3', 'data/focalizacion.py'], check=True)
+    result = subprocess.run(['python3', 'data/focalizacion.py'], check=True, capture_output=True, text=True)
     st.success("Datos procesados exitosamente.")
+    print(result.stdout)  # Imprimir salida estándar en la consola
 except subprocess.CalledProcessError as e:
-    st.error(f"Error al ejecutar focalizacion.py: {e}")
-    
-# Set the title and favicon that appear in the Browser's tab bar.
-st.set_page_config(
-    page_title='Reporte del de focalización'
-)
+    st.error(f"Error al ejecutar focalizacion.py: {e}\n{e.output}")
 
 # Cargar datos
-data = pd.read_csv('data/focalizacion_resultados.pkl')
+data = pd.read_pickle('data/focalizacion_resultados.pkl')  # Cambia a .csv si es necesario
 
 # Mostrar datos
 st.dataframe(data)
