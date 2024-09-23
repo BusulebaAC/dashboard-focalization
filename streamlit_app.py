3d71ecbddbfd41433448bd39683aff0a6624cb4c
BusulebaAC <busulebaac@gmail.com>
2024-09-23 19:21:32 +0000

    nuevo script

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,14 @@
 import streamlit as st
 import pandas as pd
+import subprocess
 
+# Ejecutar el script de limpieza
+try:
+    subprocess.run(['python3', 'data/focalizacion.py'], check=True)
+    st.success("Datos procesados exitosamente.")
+except subprocess.CalledProcessError as e:
+    st.error(f"Error al ejecutar focalizacion.py: {e}")
+    
 # Set the title and favicon that appear in the Browser's tab bar.
 st.set_page_config(
     page_title='Reporte del de focalización'
