60d265fb226437bed88151766f1d57e97c332755
BusulebaAC <busulebaac@gmail.com>
2024-09-25 18:27:31 +0000

    app

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,18 +2,22 @@ import streamlit as st
 import pandas as pd
 import subprocess
 
-# Set the title and favicon that appear in the Browser's tab bar.
+# Establecer el título
 st.title("Análisis de Datos Interactivos")
+
 # Ejecutar el script de limpieza
 try:
     result = subprocess.run(['python3', 'data/focalizacion.py'], check=True, capture_output=True, text=True)
     st.success("Datos procesados exitosamente.")
-    print(result.stdout)  # Imprimir salida estándar en la consola
 except subprocess.CalledProcessError as e:
-    st.error(f"Error al ejecutar focalizacion.py: {e}\n{e.output}")
+    st.error(f"Error al ejecutar focalizacion.py: {str(e)}\nSalida: {e.stdout}\nError: {e.stderr}")
 
-# Cargar datos
-data = pd.read_csv('data/focalizacion_resultados.csv') 
+# Cargar y mostrar los resultados
+try:
+    data = pd.read_csv('data/focalizacion_resultados.csv')
+    st.dataframe(data)
+except FileNotFoundError:
+    st.error("El archivo focalizacion_resultados.csv no se encontró. Verifica la ruta.")
+except Exception as e:
+    st.error(f"Ocurrió un error al cargar los datos: {str(e)}")
 
-# Mostrar datos
-st.dataframe(data)
